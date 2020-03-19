4538df0e34680c61c08110c1303eec00e9d5b3e8
Cameron Alvarado <cameronapple777@gmail.com>
2020-03-18 20:00:00 -0600

    commit

--- spacex-launch-app/src/components/Body.js
+++ spacex-launch-app/src/components/Body.js
@@ -11,7 +11,6 @@ import "../App.css";
 function Body({ getData, data, isFetching, error }) {
   return (
     <div className="Body">
-      {/* <ButtonEx onClick={() => setDate(today)}/> */}
       <ButtonEx getData={getData} />
       <TextBox data={data} />
       <br></br>
