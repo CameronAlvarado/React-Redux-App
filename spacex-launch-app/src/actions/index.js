fe0bab9b8e4a5d1d845e4c91cdb79aa97429af4b
CameronAlvarado <cameronapple777@gmail.com>
2020-08-13 15:45:11 -0700

    updated launch count

--- spacex-launch-app/src/actions/index.js
+++ spacex-launch-app/src/actions/index.js
@@ -4,19 +4,19 @@ export const FETCHING_DATA_START = "FETCHING_DATA_START";
 export const FETCHING_DATA_SUCCESS = "FETCHING_DATA_SUCCESS";
 export const FETCHING_DATA_FAILURE = "FETCHING_DATA_FAILURE";
 
-export const getData = () => dispatch => {
+export const getData = () => (dispatch) => {
   dispatch({ type: FETCHING_DATA_START });
   axios
     .get(
       `https://api.spacexdata.com/v3/launches/${Math.floor(
-        Math.random() * 107 + 1
+        Math.random() * 114
       )}`
     )
-    .then(res => {
+    .then((res) => {
       console.log(res);
       dispatch({ type: FETCHING_DATA_SUCCESS, payload: res.data });
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch({ type: FETCHING_DATA_FAILURE, payload: console.log(err) });
     });
 };
--- spacex-launch-app/src/reducers/index.js
+++ spacex-launch-app/src/reducers/index.js
@@ -1,36 +1,36 @@
 import {
-    FETCHING_DATA_START,
-    FETCHING_DATA_SUCCESS,
-    FETCHING_DATA_FAILURE
-  } from "../actions";
-  
-  const initialState = {
-    data: null,
-    isFetching: false,
-    error: ""
-  };
-  
-  export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case FETCHING_DATA_START:
-        return { // updates store.
-          ...state,
-          isFetching: true,
-          error: ""
-        };
-      case FETCHING_DATA_SUCCESS:
-        return {
-          ...state,
-          isFetching: false,
-          data: action.payload
-        };
-      case FETCHING_DATA_FAILURE:
-        return {
-          isFetching: false,
-          error: "Did not fetch data"
-        }
-      default:
-        return state;
-    }
-  };
-  
+  FETCHING_DATA_START,
+  FETCHING_DATA_SUCCESS,
+  FETCHING_DATA_FAILURE,
+} from "../actions";
+
+const initialState = {
+  data: null,
+  isFetching: false,
+  error: "",
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_DATA_START:
+      return {
+        // updates store.
+        ...state,
+        isFetching: true,
+        error: "",
+      };
+    case FETCHING_DATA_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+      };
+    case FETCHING_DATA_FAILURE:
+      return {
+        isFetching: false,
+        error: "Did not fetch data",
+      };
+    default:
+      return state;
+  }
+};
