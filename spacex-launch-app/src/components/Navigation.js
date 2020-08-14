fd4cedbd65aa332fdf625bc4f8f6d259b9c2bbba
CameronAlvarado <cameronapple777@gmail.com>
2020-08-14 07:02:21 -0700

    removed unused vars and now json file

--- spacex-launch-app/src/components/Navigation.js
+++ spacex-launch-app/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 
-import ButtonEx from "./ButtonEx.js";
+// import ButtonEx from "./ButtonEx.js";
 
 const Navigation = () => {
   return (
